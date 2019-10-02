5f2fe2fda4400f005c617c51f8ae9af28a9589cc
Faizan Khan <55961574+ByFaizan@users.noreply.github.com>
2019-10-02 23:09:16 +0500

    Update
    "Uncommented" The Whole Code

--- BCC031419-Asg2/Chapter 12-13/index.js
+++ BCC031419-Asg2/Chapter 12-13/index.js
@@ -1,5 +1,4 @@
 //1 checking the letters
-/*
 var character = prompt ("type a letter");
 var checkcharacter = character.charCodeAt(0);
 if (checkcharacter >= 48 && checkcharacter <= 57) {
@@ -8,11 +7,10 @@ if (checkcharacter >= 48 && checkcharacter <= 57) {
     alert("You've Entered An UPPERCASE-LETTER");
 } else if (checkcharacter >= 97 && checkcharacter <= 122) {
     alert("You've Entered A LOWERCASE-LETTER");
-}*/
+}
 
 
 //2**  accept two integers and display the larger.
-/*
 var a = prompt ("type a number");
 var b = prompt("type another number");
   if (a > 0 && b > 0 && a > b) {
@@ -26,12 +24,11 @@ var b = prompt("type another number");
   } 
   else {
     document.write("Please add an integer!");
-}*/
+}
 
 
 
 //3**  input a number and show the number is positive, negative or zero.
-/*
 var num = prompt ("Enter A Number To Check If It's\nPositive, Negative, Zero.", 0);
 if (num > 0){
 	alert(num + " is Positive number");
@@ -44,11 +41,10 @@ else if (num == 0 && -num == 0){
 }
 else {
 	alert("please type numeric only!");
-}*/
+}
 
 
 //4** input string of length 1 and returns true if it is a vowel, false otherwise
-/*
 var vowels = prompt ("Type a word");
 vowels = vowels.toLowerCase();
 if (vowels === "a" || vowels === "e" || vowels === "i" || vowels === "o" || vowels === "u"){
@@ -56,11 +52,10 @@ if (vowels === "a" || vowels === "e" || vowels === "i" || vowels === "o" || vowe
 }
 else {
 	document.write("false! its not vowel");
-}*/
+}
 
 
 //5** input pass and validate it
-/*
 var pass = "12345"
 var passInput = prompt ("Please Enter Your Password");
 if (passInput === pass){
@@ -68,11 +63,10 @@ if (passInput === pass){
 }
 else {
 	alert("Incorrect password! \nTry Again.");
-}*/
+}
 
 
 //6** if/else statement try to fix it
-/*
 var greeting;
 var hour = 13;
 if (hour < 18) {
@@ -81,11 +75,10 @@ if (hour < 18) {
 } else {
     greeting = "Good evening";
     alert(greeting);
-}*/
+}
 
 
 //7**  input from user in 24hours format, Implement case using if, else & else if 
-/*
 var time = prompt ("Enter Current Time\n(24-HR Format)");
 if (time >= 0000 && time < 1200 ){
 	alert("Good Morning");
@@ -101,6 +94,6 @@ else if (time >= 2100 && time <= 2359){
 }
 else {
 	alert("wrong input");
-}*/
+}
 
 
